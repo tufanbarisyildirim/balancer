30d1a6a30e631a9deb0c5f3263c8f394e8bceea3
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2020-04-26 14:03:57 +0300

    LeastTime policy implemented

--- leasttime.go
+++ leasttime.go
@@ -2,3 +2,20 @@ package balancer
 
 //LeastTime select a node with fastest response time
 type LeastTime struct{}
+
+//SelectNode select next node has least response time in queue
+func (lt *LeastTime) SelectNode(balancer *Balancer, clientID string) Node {
+	var selectedNode Node
+
+	for _, upstream := range balancer.UpstreamPool {
+		if !upstream.IsHealthy() { //no one needs unhealth nodes.
+			continue
+		}
+
+		if selectedNode == nil || selectedNode.GetAverageResponseTime() > upstream.GetAverageResponseTime() {
+			selectedNode = upstream
+			continue
+		}
+	}
+	return selectedNode
+}
---
+++ leasttime_test.go
@@ -0,0 +1,119 @@
+package balancer
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestLeastTime_SelectNode(t *testing.T) {
+	type args struct {
+		balancer *Balancer
+		clientID string
+	}
+	tests := []struct {
+		name string
+		lt   *LeastTime
+		args args
+		want Node
+	}{
+		{
+			name: "select the one and only we have",
+			lt:   &LeastTime{},
+			args: args{
+				balancer: &Balancer{
+					UpstreamPool: []Node{
+						&Upstream{
+							Healthy: true,
+							Host:    "127.0.0.1",
+						},
+					},
+				},
+				clientID: "127.0.0.1",
+			},
+			want: &Upstream{
+				Healthy: true,
+				Host:    "127.0.0.1",
+			},
+		},
+		{
+			name: "select the one that has least connection response time",
+			lt:   &LeastTime{},
+			args: args{
+				balancer: &Balancer{
+					UpstreamPool: []Node{
+						&Upstream{
+							Healthy:          true,
+							Host:             "127.0.0.1",
+							Load:             2,
+							RequestCount:     1000,
+							TotalRequestTime: uint64(time.Second * 10),
+						},
+						&Upstream{
+							Healthy:          true,
+							Host:             "127.0.0.2",
+							Load:             1,
+							RequestCount:     1000,
+							TotalRequestTime: uint64(time.Second * 5),
+						},
+					},
+				},
+				clientID: "127.0.0.1",
+			},
+			want: &Upstream{
+				Healthy:          true,
+				Host:             "127.0.0.2",
+				Load:             1,
+				RequestCount:     1000,
+				TotalRequestTime: uint64(time.Second * 5),
+			},
+		},
+		{
+			name: "select the one that have least connection time and healthy",
+			lt:   &LeastTime{},
+			args: args{
+				balancer: &Balancer{
+					UpstreamPool: []Node{
+						&Upstream{
+							Healthy:          true,
+							Host:             "127.0.0.1",
+							Load:             2,
+							RequestCount:     5000,
+							TotalRequestTime: uint64(time.Second * 5),
+						},
+						&Upstream{
+							Healthy:          true,
+							Host:             "127.0.0.2",
+							Load:             1,
+							RequestCount:     5000,
+							TotalRequestTime: uint64(time.Second * 4),
+						},
+						&Upstream{
+							Healthy:          false,
+							Host:             "127.0.0.3",
+							Load:             0,
+							RequestCount:     5000,
+							TotalRequestTime: 100,
+						},
+					},
+				},
+				clientID: "127.0.0.1",
+			},
+			want: &Upstream{
+				Healthy:          true,
+				Host:             "127.0.0.2",
+				Load:             1,
+				RequestCount:     5000,
+				TotalRequestTime: uint64(time.Second * 4),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			lt := &LeastTime{}
+			if got := lt.SelectNode(tt.args.balancer, tt.args.clientID); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LeastTime.SelectNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
