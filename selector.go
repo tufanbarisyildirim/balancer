9d55e56fedd559a9ac6ddf8cf859415c9c747842
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2020-04-26 17:40:15 +0300

    fix selector interface

--- selector.go
+++ selector.go
@@ -2,5 +2,5 @@ package balancer
 
 //Selector Interface, policy to decide target
 type Selector interface {
-	SelectNode(balancer *Balancer, clientID string) *Node
+	SelectNode(balancer *Balancer, clientID string) Node
 }
